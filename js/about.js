0f6e1175e67bc8889524f8229b73c98d9a2bbd01
UltraRaptorYT <62703912+UltraRaptorYT@users.noreply.github.com>
2021-11-20 20:22:13 +0800

    updated background

---
+++ js/about.js
@@ -0,0 +1,17 @@
+function start() {
+  var maxheight = document.body.scrollHeight;
+  var maxwidth = document.body.scrollWidth;
+  document.body.style.backgroundImage = `url(../src/about_top_left.png),
+    url(../src/about_top_right.png), url(../src/about_middle_left.png),
+    url(../src/about_bottom_right.png)`;
+  var scale = 40;
+  document.body.style.backgroundRepeat = "no-repeat";
+  document.body.style.backgroundAttachment = "scroll";
+  document.body.style.backgroundSize = `45%, 25%, 30%, ${scale}%`;
+  var image = new Image();
+  image.src = "../src/about_bottom_right.png";
+  console.log(image.height);
+  document.body.style.backgroundPosition = `0px 0px, right 0px,0px ${
+    maxheight / 2
+  }px,right ${maxheight - image.height * (scale / 100)}px`;
+}
