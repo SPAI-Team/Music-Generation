25a38702e6a038873be974b663fce5c0c367fd87
Oh Tien Cheng <tiencheng.oh@gmail.com>
2021-12-13 19:11:10 +0800

    [feat] Add reset settings button

--- js/settings/settings.js
+++ js/settings/settings.js
@@ -1,11 +1,26 @@
+const SETTINGS = document.getElementById("settings");
 const SLIDER = document.getElementById("temperature");
 const SHOW_NOTES = document.getElementById("show_note");
-SLIDER.addEventListener("mousemove", function () {
+
+const slider_func = () => {
     AI.temperature = parseFloat(SLIDER.value);
-});
-
-
-SHOW_NOTES.onclick = (event) => {
+};
+const show_notes_func = (event) => {
     KEYBOARD._interface.show_notes = event.srcElement.checked;
     KEYBOARD._resize();
 };
+
+const reset = () => {
+    slider_func();
+    KEYBOARD._interface.show_notes = false;
+    KEYBOARD._resize();
+}
+
+const test = () => {
+    console.log("yeet");
+};
+
+SLIDER.onchange = slider_func;
+SHOW_NOTES.onchange = show_notes_func;
+
+SETTINGS.onreset = reset
