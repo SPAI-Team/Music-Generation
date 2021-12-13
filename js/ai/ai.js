fc6b1592487a30a351e85a7b04667c8fcc219657
Oh Tien Cheng <tiencheng.oh@gmail.com>
2021-12-13 18:05:15 +0800

    [feat] Add help text on piano page

--- js/ai/ai.js
+++ js/ai/ai.js
@@ -1,3 +1,4 @@
+
 class AI_Model extends EventEmitter {
     constructor(model_config = "https://storage.googleapis.com/download.magenta.tensorflow.org/tfjs_checkpoints/music_rnn/chord_pitches_improv", temperature = 1.1) {
         super();
@@ -7,6 +8,7 @@ class AI_Model extends EventEmitter {
         this.notes = []; // Store keys played in a buffer
         this.running = false;
         this.stopCurrentSequenceGenerator;
+        this.firstTime = false;
     }
 
     buildNoteSequence(seed) {
@@ -128,7 +130,10 @@ class AI_Model extends EventEmitter {
         setTimeout(generateNext, launchWaitTime * 1000);
         let consumeNext = this.consumeNext.bind(this);
         let consumerId = Tone.Transport.scheduleRepeat(consumeNext, playIntervalTime, Tone.Transport.seconds + launchWaitTime);
-
+        if (!this.firstTime) {
+            this.firstTime = true;
+            document.getElementById("help-text").classList.add("fade")
+        }
         return () => {
             this.running = false;
             Tone.Transport.clear(consumerId);
